23b51777a619b70bc8da13ce81a7e2fe159eb1ba
nachogs13 <79532376+nachogs13@users.noreply.github.com>
2023-01-12 11:57:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(
     ],
     classifiers = [
         'Topic :: Software Development :: Documentation',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
